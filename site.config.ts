683bdac1b560c31efe0dc5282073e25962c863e5
phar-ngocson <123302719+phar-ngocson@users.noreply.github.com>
2024-01-18 17:00:00 +0000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,28 +3,28 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: 'niviki-com-t-do-t-i-ch-nh-10f5e54221eb4b059bc5b5305da949ba',
-  rootNotionPageId: 'Simple-Template-49955b133eb843bdb8665534ecfd844c',
+  rootNotionPageId: 'Blog-Post-75160d74dc6942b1b378da62bda2d5f9?pvs=4',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'NIVIKI.COM - TỰ DO TÀI CHÍNH',
-  domain: 'niviki.com',
-  author: 'Khoa Nguyen',
+  name: 'Chuyện sao Hoả',
+  domain: 'nguyenngocson.id.vn',
+  author: 'NgocSon',
 
   // open graph metadata (optional)
-  description: 'Lan toả tinh thần tự do tài chính đến với nhiều người hơn nữa! Tự do tài chính là bạn được quyền “thất nghiệp” nhưng vẫn có đủ tiền cho một cuộc sống thoải mái trong thời gian dài',
+  description: 'Thực ra tôi đến từ sao Hoả, bạn đến từ sao Kim. Chúng ta cùng sống trên Trái Đất',
 
   // social usernames (optional)
-  twitter: 'khoa_nvk',
+  //twitter: 'khoa_nvk',
   // github: 'transitive-bullshit',
-  linkedin: 'khoanguyen08',
+  //linkedin: 'khoanguyen08',
   
-  facebook: 'https://www.facebook.com/groups/nocodevietnam',
+  facebook: 'https://www.facebook.com/ChuyensaoHoa',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  //youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
